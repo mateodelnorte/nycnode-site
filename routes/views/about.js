02b11acd1a6dfd1c4d8bea733a2879e078ac31be
Matt Walters <mattwalters5@gmail.com>
2016-07-27 21:41:00 -0400

    little cleanup

--- routes/views/about.js
+++ routes/views/about.js
@@ -9,11 +9,11 @@ exports = module.exports = function(req, res) {
 	locals.page.title = 'About NYCNode';
 	
 	locals.organisers = [
-		{ name: 'Craig Sharkie', image: '/images/organiser-craig_sharkie.jpg', twitter: 'twalve',       title: 'Founder, MC, coordinator' },
+		{ name: 'Matt Walters', image: 'https://avatars3.githubusercontent.com/u/554847?v=3&s=460', twitter: 'mateodelnorte',       title: 'Lead Organizer' },
 		// { name: 'Thinkmill', image: '/images/organiser-thinkmill.jpg',     twitter: 'thethinkmill', title: 'Site coordinator' },
-		{ name: 'Gil Davidson',     image: '/images/organiser-gil_davidson.jpg',     twitter: 'iamnotyourbroom',   title: 'Atlassian coordinator' },
-		{ name: 'Adam Ahmed',    image: '/images/organiser-adam_ahmed.jpg',    twitter: 'hitsthings',   title: 'Atlassian coordinator' },
-		{ name: 'Lachlan Hardy', image: '/images/organiser-lachlan_hardy.jpg', twitter: 'lachlanhardy', title: 'Community coordinator' }
+		// { name: 'Gil Davidson',     image: '/images/organiser-gil_davidson.jpg',     twitter: 'iamnotyourbroom',   title: 'Atlassian coordinator' },
+		// { name: 'Adam Ahmed',    image: '/images/organiser-adam_ahmed.jpg',    twitter: 'hitsthings',   title: 'Atlassian coordinator' },
+		// { name: 'Lachlan Hardy', image: '/images/organiser-lachlan_hardy.jpg', twitter: 'lachlanhardy', title: 'Community coordinator' }
 	]
 	
 	view.render('site/about');
